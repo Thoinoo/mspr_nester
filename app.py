a013378000655b4f3f6da616d518179cf2d55810
antoine <thoino@outlook.fr>
2025-03-31 20:10:06 +0200

    suppression du print avec les secrets

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
-print(f"DB_USER={DB_USER}, DB_PASSWORD={DB_PASSWORD}, DB_HOST={DB_HOST}, DB_NAME={DB_NAME}")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base: DeclarativeMeta = declarative_base()
